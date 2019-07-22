a8ef2afdde0f132675acb11805618b33804bf415
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-07-22 11:41:01 -0500

    Updates

---
+++ precache-manifest.9ef0d59ad6859136890b17315b6988ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84eadb273698d600d67cff88c785ecf",
+    "url": "/chatbox-React/index.html"
+  },
+  {
+    "revision": "f84213ef30db2581d4b4",
+    "url": "/chatbox-React/static/css/main.5ebcc638.chunk.css"
+  },
+  {
+    "revision": "31b44a4e152f8a6d3f1c",
+    "url": "/chatbox-React/static/js/2.b1a0bb69.chunk.js"
+  },
+  {
+    "revision": "f84213ef30db2581d4b4",
+    "url": "/chatbox-React/static/js/main.868dbc30.chunk.js"
+  },
+  {
+    "revision": "790694ba44027bc2c23d",
+    "url": "/chatbox-React/static/js/runtime~main.b4161410.js"
+  }
+]);
